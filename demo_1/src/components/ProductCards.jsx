1f11f7c0bbab9450797b0f4451f6b766b39a7fd6
Giada Unger <giada.unger02@gmail.com>
2024-02-02 12:43:47 +0100

    Made some Conditions and  ternary

--- demo_1/src/components/ProductCards.jsx
+++ demo_1/src/components/ProductCards.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
 
 function ProductCards() {
+
+    function addStar(student) {
+        if(student === "Tobias") {
+            return <div>*</div>
+        } else if(student === "Giada") {
+            return <div>**</div>
+        } else {
+            return <div>*******</div>
+        }
+    }
+
+    const products = [
+        {
+          id: 1,
+          name: "T-Shirt 1",
+          price: 19.99,
+          features: ["100% Cotton", "Lightweight", "Unisex"],
+          isInStock: true,
+          imageUrl: "https://www.pexels.com/photo/2559941/download/", // Placeholder for direct image URL
+        },
+        {
+          id: 2,
+          name: "T-Shirt 2",
+          price: 24.99,
+          features: ["Polyester Blend", "Breathable", "Unisex"],
+          isInStock: false,
+          imageUrl: "https://www.pexels.com/photo/2559941/download/", // Placeholder for direct image URL
+        },
+        {
+          id: 3,
+          name: "T-Shirt 3",
+          price: 69.99,
+          features: ["Wool", "Breathable", "Female"],
+          isInStock: true,
+          imageUrl: "https://www.pexels.com/photo/2559941/download/", // Placeholder for direct image URL
+        },
+        // Add more products as needed
+      ];
+
+      let students = ["Melanie", "Giada", "Omar", "Tobias"]
+
   return (
-    <div>ProductCards</div>
+    <>
+        {
+            students.map((student) => {
+                return <div className="p-8 border shadow-lg m-5">
+                    {`Hello ${student}`}
+                    {student === "Tobias" && <span> !!!</span>}
+                    {student === "Tobias" ? <div>Teacher</div> : <div>Student</div>}
+                    {addStar(student)}
+                </div>
+            })
+        }
+    </>
   )
 }
 
