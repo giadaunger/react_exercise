1b61d1994b0c0fe0cddadd22e9eb0bc624af9626
Giada Unger <giada.unger02@gmail.com>
2024-02-02 11:32:56 +0100

    Added a new component and deletet the login button component

--- demo_1/src/App.jsx
+++ demo_1/src/App.jsx
@@ -1,11 +1,43 @@
-import { useState } from 'react'
-import LoginButton from './components/LoginButton'
+import React from 'react';
+import ProductCards from './components/ProductCards';
 
 function App() {
 
+  const products = [
+    {
+      id: 1,
+      name: "T-Shirt 1",
+      price: 19.99,
+      features: ["100% Cotton", "Lightweight", "Unisex"],
+      isInStock: true,
+      imageUrl: "https://www.pexels.com/photo/2559941/download/", // Placeholder for direct image URL
+    },
+    {
+      id: 2,
+      name: "T-Shirt 2",
+      price: 24.99,
+      features: ["Polyester Blend", "Breathable", "Unisex"],
+      isInStock: false,
+      imageUrl: "https://www.pexels.com/photo/2559941/download/", // Placeholder for direct image URL
+    },
+    {
+      id: 3,
+      name: "T-Shirt 3",
+      price: 69.99,
+      features: ["Wool", "Breathable", "Female"],
+      isInStock: true,
+      imageUrl: "https://www.pexels.com/photo/2559941/download/", // Placeholder for direct image URL
+    },
+    // Add more products as needed
+  ];
+
   return (
-    <div>
-      <LoginButton />
+    <div className='flex flex-col min-h-screen bg-gray-200'>
+      <header className='h-20 border-b'>Header</header>
+      <div className='flex-auto'>
+        <div className='container mx-auto h-screen'></div>
+      </div>
+      <footer className='h-20 border-t bg-gray-200'>Footer</footer>
     </div>
   )
 }
--- demo_1/src/components/LoginButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function LoginButton() {
-  return (
-    <div className='bg-red-700'>Login Button</div>
-  )
-}
-
-export default LoginButton
