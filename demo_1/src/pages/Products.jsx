d216c689ff30d5757d133726fe5275d212c9098a
Giada Unger <giada.unger02@gmail.com>
2024-02-07 15:25:28 +0100

    Fetched data from api

--- demo_1/src/pages/Products.jsx
+++ demo_1/src/pages/Products.jsx
@@ -1,22 +1,51 @@
-import React, { useState } from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Products() {
+  const [shopProducts, setShopProducts] = useState([]);
 
-    const [nameInput, setNameInput] = useState("")
+  async function handleSearch(event) {
+    event.preventDefault();
 
-    return (
-        <div className="flex flex-col min-h-screen">
-            <div className="flex-auto">
-                <h1>Products</h1>
-                <div className="container mx-auto p-10">
-                    <input type="input" placeholder="Enter your name" onChange={(event) => setNameInput(event.target.value)} className="border border-black" />
+    try{
+        const response = await fetch("https://dummyjson.com/products");
+        const data = await response.json();
+        setShopProducts(data["products"]);
+    }
+    catch(error) {
+        console.log(error)
+    }
+    
+  }
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-auto">
+        <h1 className="text-7xl">Products</h1>
+        <div className="container mx-auto">
+          <form onSubmit={handleSearch} className="my-10">
+            <button
+              type="submit"
+              className="px-4 py-2 font-semibold text-white bg-black"
+            >
+              Fetch products
+            </button>
+          </form>
+          <div className="container grid grid-cols-3 mx-auto">
+            {shopProducts.map((product) => {
+              return (
+                <div className="p-8 border shadow-lg m-4">
+                  <div>{product.title}</div>
+                  <div>{product.price} $</div>
                 </div>
-                <div>
-                    {nameInput}
-                </div>
-            </div>
+              );
+            })}
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Products
+export default Products;
